ae15356716f1bf08c8c52bd8970153e9fe0323c3
shaoleis <shaoleishen@gmail.com>
2016-11-16 20:57:44 -0500

    Update main.m

--- code/main.m
+++ code/main.m
@@ -1,10 +1,3 @@
-//
-//  main.m
-//  CvVideoCamera_Example
-//
-//  Created by Simon Lucey on 10/1/16.
-//  Copyright © 2016 CMU_16623. All rights reserved.
-//
 
 #import <UIKit/UIKit.h>
 #import "AppDelegate.h"
