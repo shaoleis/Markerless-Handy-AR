21421c68b6285e9ebb668b06dfbe2eaf849a932b
shaoleis <shaoleishen@gmail.com>
2016-11-16 20:40:54 -0500

    Add files via upload

---
+++ code/ViewController.h
@@ -0,0 +1,20 @@
+//
+//  ViewController.h
+//  CvVideoCamera_Example
+//
+//  Created by Simon Lucey on 10/1/16.
+//  Copyright © 2016 CMU_16623. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <opencv2/highgui/ios.h>
+
+// Slightly changed things here to employ the CvVideoCameraDelegate
+@interface ViewController : UIViewController<CvVideoCameraDelegate>
+{
+    CvVideoCamera *videoCamera; // OpenCV class for accessing the camera
+}
+// Declare internal property of videoCamera
+@property (nonatomic, retain) CvVideoCamera *videoCamera;
+
+@end
