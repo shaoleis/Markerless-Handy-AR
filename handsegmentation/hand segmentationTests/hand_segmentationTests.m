550df6d123cbe0287c9f51b6f23d776928931ac8
miaol1 <lmaptx4869@gmail.com>
2016-11-17 20:40:40 -0500

    hand segmentation beta 2

---
+++ handsegmentation/hand segmentationTests/hand_segmentationTests.m
@@ -0,0 +1,39 @@
+//
+//  hand_segmentationTests.m
+//  hand segmentationTests
+//
+//  Created by 刘淼 on 11/15/16.
+//  Copyright © 2016 刘淼. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface hand_segmentationTests : XCTestCase
+
+@end
+
+@implementation hand_segmentationTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
